d7a743dab12f90edeece28aa9d8181860fbcd2ed
henry <henry.waweru@student.moringaschool.com>
2023-04-05 10:03:26 +0300

    completed

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ let random = Math.ceil(Math.random() * 10);
 
 let num3 = 10;
 let num4 = 6;
-let mod = num3 % num4;
+let mod = num3%num4;
 
-let nums = [2, 6, 8, 12, 14, 16, 18, 20];
+let nums = [2,4,6, 8, 10, 12, 14, 16, 18, 20];
 let max = Math.max.apply(null, nums);
+
+
+
+console.log(mode);
